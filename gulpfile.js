7b64df58a59bed3864c7f8bdc762d284a6bfc6c2
Leerane <leeranez@gmail.com>
2018-04-20 05:28:38 +0300

    Changes to project

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,67 @@
 "use strict";
 
+// Name variables
+
+var name = {
+  jsFile: "main.js",
+  cssFile: "styles.css",
+  scssFile: "styles.scss",
+  libsFile: "libs.js",
+  configFile: "_config.scss"
+};
+
 // Path variables
+
 var path = {
   sourcePath: "./source",
   buildPath: "./build",
-  scssPath: "/scss",
+  scssPath: "/sass",
   cssPath: "/css",
+  jsPath: "/js",
+  jsModulesPath: "/modules",
+  libsPath: "/libs",
   blocksPath: "/blocks",
+  utilityPath: "/utility",
   scssPattern: "/**/*.{scss, sass}",
+  _scssPattern: "/**/_*.{scss, sass}",
+  _configFilePattern: "/" + name.configFile,
+  scssFilePattern: "/" + name.scssFile,
   pugPattern: "/**/!(_)*.pug",
-  htmlPattern: "/**/*.html",
+  htmlPattern: "/**/!(_)*.html",
+  jsPattern: "/**/!(_)*.js",
   levels: ["blocks"]
 };
 
 // Packages
+
 var gulp = require("gulp"),
   sass = require("gulp-sass"),
+  postcss = require("gulp-postcss"),
+  posthtml = require("gulp-posthtml"),
   csscomb = require("gulp-csscomb"),
   pug = require("gulp-pug"),
   sourcemaps = require("gulp-sourcemaps"),
   htmlbeautify = require("gulp-html-beautify"),
   gulpBemCss = require("gulp-bem-css"),
-  browserSync = require("browser-sync").create();
+  concat = require("gulp-concat"),
+  merge = require("gulp-merge"),
+  cheerio = require("gulp-cheerio"),
+  plumber = require("gulp-plumber"),
+  rename = require("gulp-rename"),
+  replace = require("gulp-replace"),
+  uglify = require("gulp-uglify"),
+  svgsprite = require("gulp-svg-sprites"),
+  svgmin = require("gulp-svgmin"),
+  jsbeautify = require("js-beautify"),
+  mergeStream = require("merge-stream"),
+  autoprefixer = require("autoprefixer"),
+  cssnano = require("cssnano"),
+  browserSync = require("browser-sync").create(),
+  reload = browserSync.reload;
 
-gulp.task("browser-sync", function() {
+// Tasks
+
+gulp.task("browser-sync", () => {
   browserSync.init({
     server: {
       baseDir: "./"
@@ -33,35 +71,89 @@ gulp.task("browser-sync", function() {
   });
 });
 
-gulp.task("sass", function () {
-  return gulp.src(path.sourcePath + path.scssPath + path.scssPattern)
+gulp.task("sass-concat", () => {
+  return gulp.src(
+    [path.sourcePath + path.scssPath + path.utilityPath + path._configFilePattern,
+      path.sourcePath + path.scssPath + path.blocksPath + path._scssPattern]
+  )
+    .pipe(plumber())
     .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: "expanded"}).on("error", sass.logError))
+    .pipe(concat(name.scssFile))
     .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest(path.sourcePath + path.cssPath))
-    .pipe(browserSync.stream());
+    .pipe(gulp.dest(path.sourcePath + path.scssPath))
+    .pipe(reload({ stream: true }));
 });
 
-gulp.task("sort-sass", function () {
+gulp.task("sass-styles", ["sass-concat"], () => {
+  return gulp.src(path.sourcePath + path.scssPath + path.scssPattern)
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: "expanded"}).on("error", sass.logError))
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(csscomb("csscomb.json"))
+    .pipe(gulp.dest(path.sourcePath + path.cssPath))
+    .pipe(postcss([ cssnano({ minifyFontWeight: false }) ]))
+    .pipe(rename({
+      suffix: ".min",
+      extname: ".css"
+    }))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest(path.sourcePath + path.cssPath))
+    .pipe(reload({ stream: true }));
+});
+
+gulp.task("css", ["sass-concat", "sass-styles"]);
+
+gulp.task("js", () => {
+  return gulp.src(path.sourcePath + path.jsPath + path.jsModulesPath + path.jsPattern, { base: process.cwd() })
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(concat(name.jsFile))
+    .pipe(gulp.dest(path.sourcePath + path.jsPath))
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: ".min",
+      extname: ".js"
+    }))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest(path.sourcePath + path.jsPath))
+    .pipe(reload({ stream: true }));
+});
+
+gulp.task("libs-js", () => {
+  return gulp.src(path.sourcePath + path.libsPath  + path.jsPattern, { base: process.cwd() })
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(concat(name.libsFile))
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: ".min",
+      extname: ".js"
+    }))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest(path.sourcePath + path.jsPath))
+});
+
+gulp.task("sort-sass", () => {
   return gulp.src(path.sourcePath + path.scssPath + path.scssPattern)
     .pipe(csscomb("csscomb.json"))
     .pipe(gulp.dest(path.sourcePath + path.scssPath))
 });
 
-gulp.task("sort-html", function () {
+gulp.task("sort-html", () => {
   gulp.src(path.sourcePath + path.htmlPattern)
     .pipe(htmlbeautify())
-    .pipe(gulp.dest(path.sourcePath))
+    .pipe(gulp.dest(path.buildPath))
 });
 
-gulp.task("pug", function () {
+gulp.task("pug", () => {
   return gulp.src(path.sourcePath + path.pugPattern)
     .pipe(pug({pretty: true}))
     .pipe(gulp.dest(path.buildPath))
-    .pipe(browserSync.stream());
+    .pipe(reload({ stream: true }));
 });
 
-gulp.task("bem-html", function () {
+gulp.task("bem-css", () => {
   gulp.src(path.sourcePath + path.htmlPattern)
     .pipe(gulpBemCss({
       folder: path.sourcePath + path.scssPath + path.blocksPath,
@@ -72,9 +164,10 @@ gulp.task("bem-html", function () {
     .pipe(gulp.dest(path.sourcePath))
 });
 
-gulp.task("watch", ["browser-sync", "sass"], function() {
+gulp.task("server", ["browser-sync", "css", "js", "libs-js"], function() {
   gulp.watch(path.sourcePath + path.scssPath + path.scssPattern, ["sass"]);
-  gulp.watch(path.sourcePath + path.htmlPattern).on("change", browserSync.reload);
+  gulp.watch(path.sourcePath + path.jsPath + path.jsModulesPath + path.jsPattern, ["js"]);
+  gulp.watch(path.sourcePath + path.htmlPattern).on("change", reload);
 });
 
 gulp.task("beautify", ["sort-sass", "sort-html"]);
