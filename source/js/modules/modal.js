8f1e5d2fcc5d08e7da8e3ff45e6a2367db4aa28a
Leerane <leeranez@gmail.com>
2018-05-12 00:29:52 +0300

    Finished JS stuff

--- source/js/modules/modal.js
+++ source/js/modules/modal.js
@@ -1,15 +1,26 @@
 var modalOverlay = document.querySelector(".modal-overlay"),
   modalCart = modalOverlay.querySelector(".modal-cart"),
-  modalButton = document.querySelector(".js-modal-button");
+  modalButtons = document.querySelectorAll(".js-modal-button");
+  modalButtonArray = Array.prototype.slice.call(modalButtons);
 
-modalButton.addEventListener("click", function () {
-  modalOverlay.classList.add("modal-overlay--open");
-  modalCart.classList.add("modal-cart--open");
-  document.body.style.overflow = "hidden";
-});
+  modalButtonArray.forEach(function(el) {
+    el.addEventListener("click", function (e) {
+      e.preventDefault();
+      modalOverlay.classList.remove("visually-hidden");
+      modalOverlay.classList.add("modal-overlay--open");
+      modalCart.classList.add("modal-cart--open");
+      document.body.style.overflow = "hidden";
+      if (modalCart.classList.contains("modal-cart--open")) {
+        window.addEventListener("click", function (e) {
+          if (e.target === modalOverlay) {
+            document.body.style.overflow = "visible";
+            modalOverlay.classList.remove("modal-overlay--open");
+            modalCart.classList.remove("modal-cart--open");
+          }
+        });
+      }
+    });
+  });
 
-/*window.addEventListener("click", function (e) {
-  if (e.target !== modalCart) {
-    modalOverlay.classList.add("visually-hidden");
-  }
-});*/
+
+
